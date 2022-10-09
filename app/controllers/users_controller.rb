d9f9a3d7fa26117e44275a46d93072e46f1e22c7
ryan-farruggia <rjfarruggia@csus.edu>
2022-10-08 18:47:13 -0700

    update gemfile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,12 @@
 class UsersController < ApplicationController
+  before_action :set_user
+  def profile
+    @user.update(views: @user.views + 1)
+  end
 
-    before_action :set_user
+  private
 
-    def profile
-        @user.update(views: @user.views + 1)
-    end
-
-    private
-
-    def set_user
-        @user = User.find(params[:id])
-    end
-    
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
-
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,7 @@ Rails.application.routes.draw do
   resources :posts
   get 'about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
   root 'pages#home'
 end
