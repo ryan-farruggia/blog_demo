3f13957a37c0ae80011af3c3d039b7f5c5bf2747
ryan-farruggia <rjfarruggia@csus.edu>
2022-10-08 18:34:47 -0700

    tests

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-
+User.create(email: 'ryan@myjeenie.com', password: 'password', password_confirmation: 'password')
 10.times do |x|
-    Post.create(title: "Title #{x}", body: "Body #{x} Words go here")
+    Post.create(title: "Title #{x}", body: "Body #{x} Words go here", user_id: User.first.id)
 end
